3d85dcae3f6e9aa560480bf1ba65f4d4c603de1b
Rhy <415560124@qq.com>
2020-09-12 14:29:31 +0800

    提交人：任鸿毅 提交标题：easypoi demo项目 提交内容： 1.增加lombok插件

--- src/main/java/com/rhy/easypoidemo/entity/StudentEntity.java
+++ src/main/java/com/rhy/easypoidemo/entity/StudentEntity.java
@@ -2,6 +2,10 @@ package com.rhy.easypoidemo.entity;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
 import cn.afterturn.easypoi.excel.annotation.ExcelTarget;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -16,6 +20,9 @@ import java.time.LocalDateTime;
  * @modification_time：2020-09-12 13:27
  */
 @ExcelTarget("student")
+@Getter
+@Setter
+@Accessors(chain=true) // 可以链式调用 setter
 public class StudentEntity {
     public StudentEntity(Integer id, String name, int sex, LocalDate birthday, LocalDateTime registrationDate) {
         this.id = id;
@@ -28,76 +35,26 @@ public class StudentEntity {
     /**
      * id
      */
-    public Integer id;
+    private Integer id;
     /**
      * 学生姓名
      */
     @Excel(name = "学生姓名" , width = 30 , isImportField = "true")
-    public String name;
+    private String name;
     /**
      * 学生性别
      */
     @Excel(name = "学生姓名" , replace = {"男_1","女_2"} , suffix = "生" , isImportField = "true")
-    public int  sex;
+    private int  sex;
     /**
      * 生日
      */
     @Excel(name = "出生日期" , databaseFormat = "yyyyMMdd" , format = "yyyy-MM-dd" , isImportField = "true" , width = 20)
-    public LocalDate birthday;
+    private LocalDate birthday;
     /**
      * 进校日期
      */
     @Excel(name = "进校日期" ,databaseFormat = "yyyyMMddHHmmss" , format = "yyyy-MM-dd HH:mm:ss" , isImportField = "true" , width = 30)
-    public LocalDateTime registrationDate;
+    private LocalDateTime registrationDate;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSex() {
-        return sex;
-    }
-
-    public void setSex(int sex) {
-        this.sex = sex;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public LocalDateTime getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(LocalDateTime registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    @Override
-    public String toString() {
-        return "StudentEntity{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", sex=" + sex +
-                ", birthday=" + birthday +
-                ", registrationDate=" + registrationDate +
-                '}';
-    }
 }
--- src/main/java/com/rhy/easypoidemo/entity/Teacher.java
+++ src/main/java/com/rhy/easypoidemo/entity/Teacher.java
@@ -2,6 +2,9 @@ package com.rhy.easypoidemo.entity;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
 import cn.afterturn.easypoi.excel.annotation.ExcelTarget;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
@@ -14,6 +17,9 @@ import java.io.Serializable;
  * @modification_time：2020-09-12 13:14
  */
 @ExcelTarget("teacherEntry")
+@Getter
+@Setter
+@Accessors(chain=true) // 可以链式调用 setter
 public class Teacher implements Serializable {
     @Excel(name = "主讲老师_teacherEntity,代课老师_absent", orderNum = "1", mergeVertical = true,needMerge=true,isImportField = "true_major,true_absent")
     private String name;
