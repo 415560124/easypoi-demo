02457f6e063dd65ca14438477d665f6594370ad7
Rhy <415560124@qq.com>
2020-09-12 15:36:13 +0800

    提交人：任鸿毅 提交标题：easypoi demo项目 提交内容： 1.公共写入文件逻辑修改

--- src/test/java/com/rhy/EasypoiDemoApplicationTests.java
+++ src/test/java/com/rhy/EasypoiDemoApplicationTests.java
@@ -22,6 +22,7 @@ import java.util.Random;
 
 @SpringBootTest
 class EasypoiDemoApplicationTests {
+    private static final String BASE_URL = System.getProperty("user.dir")+"\\file";
     /**
      * 注解导出简单示例
      * @throws IOException
@@ -37,27 +38,17 @@ class EasypoiDemoApplicationTests {
                     i,
                     "学生姓名"+i,
                     i%2+1,
-                    null,//LocalDate.of(1996,i,i+10),
+                    LocalDate.of(1996,i,i+10),
                     LocalDateTime.now(ZoneId.of("+8"))
             );
             studentEntities.add(studentEntity);
         }
         //导出Excel文件对象
         Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("计算机一班学生","学生"),StudentEntity.class,studentEntities);
-        //设置导出目录
-        File file = new File("D:\\ideaProject\\easypoi-demo\\file");
-        //目录不存在则创建目录
-        if(!file.exists()){
-            file.mkdir();
-        }
-        //写入到文件流中  真正生成excel文件  可以想象成workbook对象还在内存中，现在才开始真正写入磁盘
-        FileOutputStream fos = new FileOutputStream("D:\\ideaProject\\easypoi-demo\\file\\annotation-student-demo.xls");
-        //将内存excel写入磁盘文件
-        workbook.write(fos);
-        //写完后关闭流
-        fos.close();
-    }
+        //写入文件逻辑
+        exportFile(workbook,"annotation-student-demo.xls");
 
+    }
     /**
      * 注解导出嵌套集合数据示例
      */
@@ -87,15 +78,34 @@ class EasypoiDemoApplicationTests {
             }
             courseEntities.add(courseEntity);
         }
-        File file = new File("D:\\ideaProject\\easypoi-demo\\file");
+
+        //导出Excel文件对象
+        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("课程列表","课程"),CourseEntity.class,courseEntities);
+        //写入文件逻辑
+        exportFile(workbook,"annotation-course-demo.xls");
+
+    }
+
+    /**
+     * 写入文件
+     * @param workbook
+     * @param fileName
+     * @throws IOException
+     */
+    private void exportFile(Workbook workbook,String fileName) throws IOException {
+        //设置导出目录
+        File file = new File(BASE_URL);
+        //目录不存在则创建目录
         if(!file.exists()){
             file.mkdir();
         }
-        //设置导出对象
-        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("课程列表","课程"),CourseEntity.class,courseEntities);
-        FileOutputStream fileOutputStream = new FileOutputStream("D:\\ideaProject\\easypoi-demo\\file\\annotation-course-demo.xls");
-        workbook.write(fileOutputStream);
-        fileOutputStream.close();
+        //写入到文件流中  真正生成excel文件  可以想象成workbook对象还在内存中，现在才开始真正写入磁盘
+        FileOutputStream fos = new FileOutputStream(BASE_URL+"\\"+fileName);
+        //将内存excel写入磁盘文件
+        workbook.write(fos);
+        //写完后关闭流
+        fos.close();
     }
+
 
 }
